4823124b63431da3b338fcc4a4f25ee4f4649004
ShoogLove <143033124+ShoogLove@users.noreply.github.com>
2023-09-13 11:42:06 +0300

    Piano Update

--- src/piano/Piano.js
+++ src/piano/Piano.js
@@ -3,21 +3,56 @@ import React, { Component } from 'react';
 // Define your piano notes and import the corresponding sound files
 const pianoNotes = [
   { note: 'C4', sound: require('./PianoA.wav') },
-  // Add more notes here
+  { note: 'C#4', sound: require('./PianoA.wav') },
+  { note: 'D4', sound: require('./PianoA.wav') },
+  { note: 'D#4', sound: require('./PianoA.wav') },
+  { note: 'E4', sound: require('./PianoA.wav') },
+  { note: 'F4', sound: require('./PianoA.wav') },
+  { note: 'F#4', sound: require('./PianoA.wav') },
+  { note: 'G4', sound: require('./PianoA.wav') },
+  { note: 'G#4', sound: require('./PianoA.wav') },
+  { note: 'A4', sound: require('./PianoA.wav') },
+  { note: 'A#4', sound: require('./PianoA.wav') },
+  { note: 'B4', sound: require('./PianoA.wav') },
+  { note: 'C5', sound: require('./PianoA.wav') }, // Second octave
+  { note: 'C#5', sound: require('./PianoA.wav') },
+  { note: 'D5', sound: require('./PianoA.wav') },
+  { note: 'D#5', sound: require('./PianoA.wav') },
+  { note: 'E5', sound: require('./PianoA.wav') },
+  { note: 'F5', sound: require('./PianoA.wav') },
+  { note: 'F#5', sound: require('./PianoA.wav') },
+  { note: 'G5', sound: require('./PianoA.wav') },
+  { note: 'G#5', sound: require('./PianoA.wav') },
+  { note: 'A5', sound: require('./PianoA.wav') },
+  { note: 'A#5', sound: require('./PianoA.wav') },
+  { note: 'B5', sound: require('./PianoA.wav') },
 ];
 
-const buttonStyle = {
+const whiteKeyStyle = {
   width: '60px',
   height: '200px',
-  margin: '5px',
+  margin: '0',
+  padding: '0',
   fontSize: '18px',
   backgroundColor: 'white',
   border: '1px solid #ccc',
+  display: 'inline-block',
+  verticalAlign: 'top', // Align the top of the buttons
+  color: 'black', // Set the text color for white keys
 };
 
-const activeButtonStyle = {
-  ...buttonStyle,
-  backgroundColor: '#ffcc00',
+const blackKeyStyle = {
+  width: '40px',
+  height: '150px',
+  margin: '0',
+  padding: '0',
+  fontSize: '18px',
+  backgroundColor: 'black',
+  color: 'white',
+  border: '1px solid #000',
+  display: 'inline-block',
+  verticalAlign: 'top', // Align the top of the buttons
+  color: 'white', // Set the text color for black keys
 };
 
 class Piano extends Component {
@@ -29,32 +64,43 @@ class Piano extends Component {
     };
   }
 
-  // Handle button click to play the note
+  // Handle button click to play the note and change the background color
   handleNoteClick = (note) => {
-    const audio = new Audio(note.sound.default);
+    const audio = new Audio(note.sound);
     audio.play();
     this.setState({ activeNote: note.note });
   };
 
   render() {
     return (
-      <div>
+      <div
+        style={{
+          display: 'flex',
+          flexDirection: 'column',
+          justifyContent: 'center', // Center both horizontally and vertically
+          alignItems: 'center', // Center both horizontally and vertically
+          height: '100vh', // Set the height of the container to the full viewport height
+        }}
+      >
         <h1>Virtual Piano</h1>
-        <div
-          style={{
-            display: 'flex',
-            justifyContent: 'center',
-            alignItems: 'flex-end',
-          }}
-        >
+        <p>Try out our new piano and create your melodies with the unique sound of MusicNet.</p>
+        <div>
           {pianoNotes.map((note) => (
             <button
               key={note.note}
               onClick={() => this.handleNoteClick(note)}
               style={
-                this.state.activeNote === note.note
-                  ? activeButtonStyle
-                  : buttonStyle
+                note.note.includes('#')
+                  ? {
+                      ...blackKeyStyle,
+                      backgroundColor: this.state.activeNote === note.note ? '#ffffcc' : 'black',
+                      color: this.state.activeNote === note.note ? 'black' : 'white',
+                    }
+                  : {
+                      ...whiteKeyStyle,
+                      backgroundColor: this.state.activeNote === note.note ? '#ffffcc' : 'white',
+                      color: this.state.activeNote === note.note ? 'black' : 'black',
+                    }
               }
             >
               {note.note}
